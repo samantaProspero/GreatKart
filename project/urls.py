dfd9a794e2db575ed0c5f7dafabc507804b0621c
Samanta Próspero <59126841+samantaProspero@users.noreply.github.com>
2023-02-09 08:57:55 -0300

    produto

--- project/urls.py
+++ project/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
