027bd9fad7010fd84aec11be9092273e30abb412
Samanta Próspero <59126841+samantaProspero@users.noreply.github.com>
2023-02-10 09:17:54 -0300

    seção 11 model e instancia de variação

--- carts/views.py
+++ carts/views.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
-from store.models import Product
+from store.models import Product, Variation
 
 from .models import Cart, CartItem
 
@@ -16,12 +16,19 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    if request.method == 'POST':
-        color = request.POST['color']
-        size = request.POST['size']
-        print(color, size)
-        
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+                print(product_variation)
+            except:
+                pass
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except:
